a51158319a64d45e3ed900956feb99b9881c8f69
franciskairos777 <22katuf@live.elida.k12.oh.us>
2021-09-24 10:02:36 -0400

    movement boundaries

--- Assets/Scripts/PlayerController01.cs
+++ Assets/Scripts/PlayerController01.cs
@@ -6,6 +6,9 @@ public class PlayerController01 : MonoBehaviour
 {
     public int score = 7;
     public float speed = 10.0f;
+    public float xRange;
+    public float yRange; 
+
 
     // Start is called before the first frame update
     void Start()
@@ -18,9 +21,37 @@ public class PlayerController01 : MonoBehaviour
         Debug.Log("Hello, World");
     }
 
+    private void LateUpdate()
+    {
+        //Keep Player within xRange (Left and Right sides)
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector2(xRange, transform.position.y);
+        }
+
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector2(-xRange, transform.position.y);
+        }
+
+        //Keep Player within yRange (Left and Right sides)
+        if (transform.position.y > yRange)
+        {
+            transform.position = new Vector2(transform.position.x, yRange);
+        }
+
+        if (transform.position.y < -yRange)
+        {
+            transform.position = new Vector2(transform.position.x, -yRange);
+        }
+
+    }
+    
+    
     // Update is called once per frame
     void Update()
     {
+        
 
         float moveHorizontal = Input.GetAxis("Horizontal");
         Debug.Log(moveHorizontal);
