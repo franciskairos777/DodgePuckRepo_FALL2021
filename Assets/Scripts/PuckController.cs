827822cf3a44b86da309fe612462937b67cc49fd
franciskairos777 <22katuf@live.elida.k12.oh.us>
2021-10-07 10:01:27 -0400

    wall boundaries for pucks

--- Assets/Scripts/PuckController.cs
+++ Assets/Scripts/PuckController.cs
@@ -7,7 +7,7 @@ public class PuckController : MonoBehaviour
     public int[] direction = { 0, 1, 2, 3 };
     public int moveDirection;
     public float puckSpeed = 5.0f;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,9 +20,32 @@ public class PuckController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(moveDirection == 0)
+        if (moveDirection == 0)
         {
             transform.Translate(transform.up * puckSpeed * Time.deltaTime);
         }
+
+        if (moveDirection == 1)
+        {
+            transform.Translate(transform.right * puckSpeed * Time.deltaTime);
+        }
+
+        if (moveDirection == 2)
+        {
+            transform.Translate(transform.up * -puckSpeed * Time.deltaTime);
+        }
+
+        if (moveDirection == 3)
+        {
+            transform.Translate(transform.right * -puckSpeed * Time.deltaTime);
+        }
+
+
     }
-}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("WALLLLLLLLLLLLLL");
+        puckSpeed = -puckSpeed;
+    }
+ }   
