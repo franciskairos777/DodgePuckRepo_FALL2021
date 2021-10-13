26c969bb1d318d4be2d3dc78536c14b8743b924e
franciskairos777 <22katuf@live.elida.k12.oh.us>
2021-10-13 10:02:34 -0400

    fixed boundaries for puck

--- Assets/Scripts/PlayerController01.cs
+++ Assets/Scripts/PlayerController01.cs
@@ -11,11 +11,11 @@ public class PlayerController01 : MonoBehaviour
     public float xRange;
     public float yRange;
     public GameObject Puck;
-
+    public GameObject Blocky; 
     // Start is called before the first frame update
     void Start()
     {
-        //Instantiate(Puck, new Vector2(Random.Range(-xRange, xRange), Random.Range(-yRange, yRange)), Quaternion.identity);
+        //Instantiate(Blocky, new Vector2(Random.Range(-xRange, xRange), Random.Range(-yRange, yRange)), Quaternion.identity);
         
 
 
@@ -52,7 +52,7 @@ public class PlayerController01 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Instantiate(Puck,new Vector2(Random.Range(-xRange,xRange), Random.Range(-yRange, yRange)), Quaternion.identity);
+        //Instantiate(Puck,new Vector2(Random.Range(-xRange,xRange), Random.Range(-yRange, yRange)), Quaternion.identity);
 
         //count how many enemies there are in the scene
         GameObject[] puckArray;
@@ -81,4 +81,6 @@ public class PlayerController01 : MonoBehaviour
         Debug.Log("My Lotto Number is: " + myNumber);
 
     }
+
+    void OnTriggerEnter(Collider other) { }
 }
